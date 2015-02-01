a82dadb61b7e088e3ce9d4464908a2de848a33e0
Alexander Gabert <alexander.gabert@gmail.com>
2015-02-01 17:09:37 +0100

    bugfix

--- misc/boot.sh
+++ misc/boot.sh
@@ -6,5 +6,5 @@ nova boot \
   --key-name "$(nova keypair-list | grep "$(hostname)_root_ssh_id_rsa_nova" | head -n1 | awk -F'|' '{print $2;}' | xargs -n1 echo)" \
   --security-groups "$(neutron security-group-list | grep testing | head -n1 | awk -F'|' '{print $2;}' | xargs -n1 echo)" \
   --nic net-id="$(neutron net-list | grep internal | head -n1 | awk -F'|' '{print $2;}' | xargs -n1 echo)" \
-    "test$(date +%%s)"
+    "test$(date +%s)"
 
