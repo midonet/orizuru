2193abd2f7c27187b9c903bf1a50659f778a4022
Alexander Gabert <alexander@midokura.com>
2015-07-03 20:01:38 +0200

    wget

--- stages/stage6/fabfile.py
+++ stages/stage6/fabfile.py
@@ -1663,7 +1663,7 @@ EOF
 
     mkdir -p /var/www/cgi-bin/keystone
 
-    curl http://git.openstack.org/cgit/openstack/keystone/plain/httpd/keystone.py?h=stable/kilo | tee /var/www/cgi-bin/keystone/main /var/www/cgi-bin/keystone/admin
+    wget -SO- http://git.openstack.org/cgit/openstack/keystone/plain/httpd/keystone.py?h=stable/kilo | tee /var/www/cgi-bin/keystone/main /var/www/cgi-bin/keystone/admin
 
     chown -R keystone:keystone /var/www/cgi-bin/keystone
     chmod 755 /var/www/cgi-bin/keystone/*
